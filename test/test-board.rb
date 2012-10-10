0a34e3ed8ae9a9c6e6ca7bd60945222f941cb8a7
Hassan Shamim <hassan.has.you@gmail.com>
2012-10-10 11:35:35 -0500

    Created mailbox boards for move-checking

--- test/test-board.rb
+++ test/test-board.rb
@@ -4,8 +4,11 @@ require 'board.rb'
 
 class BoardTest < Test::Unit::TestCase
 
-  def test_01_some stuff
-
+  def test_01_board_has_both_mailboxes
+    m = Board.new
+    
+    assert_equal true, !!m.mailbox
+    assert_equal true, !!m.mailbox64
   end
 
 end
