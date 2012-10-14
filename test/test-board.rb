404f1191d49ed71ae5d79b798866c2072c2472f9
Hassan Shamim <hassan.has.you@gmail.com>
2012-10-14 15:31:17 -0500

    trashed previous work with board

--- test/test-board.rb
+++ test/test-board.rb
@@ -4,11 +4,5 @@ require 'board.rb'
 
 class BoardTest < Test::Unit::TestCase
 
-  def test_01_board_has_both_mailboxes
-    m = Board.new
-    
-    assert_equal true, !!m.mailbox
-    assert_equal true, !!m.mailbox64
-  end
 
 end
