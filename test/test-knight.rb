241bd607674489e85916457a856bb1e293392341
Hassan Shamim <hassan.has.you@gmail.com>
2012-10-18 11:21:08 -0500

    renamed some test variables to be more expressive

--- test/test-knight.rb
+++ test/test-knight.rb
@@ -4,26 +4,26 @@ require 'knight.rb'
 class KnightTest < Test::Unit::TestCase
 
   def test_01_knight_inherits_gamepiece_initializiation
-    p = Knight.new( :black, 2, 1 )
+    knight = Knight.new( :black, 2, 1 )
 
-    assert_equal :black , p.color
-    assert_equal 2, p.file
-    assert_equal 1, p.rank
-    assert p.active?
+    assert_equal :black , knight.color
+    assert_equal 2, knight.file
+    assert_equal 1, knight.rank
+    assert knight.active?
   end
 
   def test_02a_Knight_initial_returns_a_string
-    p = Knight.new( :black, 2, 1 )   
+    knight = Knight.new( :black, 2, 1 )   
     
-    knight_initial = p.piece_initial
+    knight_initial = knight.piece_initial
     
     assert knight_initial.is_a? String
   end
 
   def test_02b_Knight_initial_is_N
-    p = Knight.new( :black, 2, 1 )   
+    knight = Knight.new( :black, 2, 1 )   
 
-    knight_initial = p.piece_initial
+    knight_initial = knight.piece_initial
     assert_equal 'N', knight_initial
   end
 
@@ -36,40 +36,40 @@ class KnightTest < Test::Unit::TestCase
   end
 
   def test_5a_valid_move_coords_is_2d_array
-    n = Knight.new( :black, 1, 1)
+    knight = Knight.new( :black, 1, 1)
 
-    coords = n.valid_move_coords
+    coords = knight.valid_move_coords
 
     assert_equal Array , coords.class
     assert_equal Array, coords.first.class
   end
 
   def test_5b_valid_move_coords_returns_8_coords_max
-    n = Knight.new( :black, 3, 3)
-    coords = n.valid_move_coords
+    knight = Knight.new( :black, 3, 3)
+    coords = knight.valid_move_coords
 
     assert_equal 8,  coords.size
   end
 
   def test_5c_valid_move_coords_doesnt_include_starting_position
-    r = Knight.new( :black, 2, 2)
-    coords = r.valid_move_coords
+    knight = Knight.new( :black, 2, 2)
+    coords = knight.valid_move_coords
 
     assert_equal false, coords.include?(r.position)
   end
 
   def test_5d_valid_move_coords_doesnt_go_off_board
-    r = Knight.new( :white, 1, 1)
-    coords = r.valid_move_coords.sort
+    knight = Knight.new( :white, 1, 1)
+    coords = knight.valid_move_coords.sort
     expected = [ [0,3], [2,3], [3,0], [3,2] ]
 
     assert_equal expected, coords
   end
 
   def test_6_valid_move_coords_is_valid_attack_coords
-    r = Knight.new( :white, 3, 3 )
-    move_coords = r.valid_move_coords
-    attack_coords = r.valid_attack_coords
+    knight = Knight.new( :white, 3, 3 )
+    move_coords = knight.valid_move_coords
+    attack_coords = knight.valid_attack_coords
 
     assert_equal move_coords, attack_coords
   end
